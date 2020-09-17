0b83abfb3daaeab1188668fe36be6c05e61c7763
EC2 Default User <ec2-user@ip-172-16-190-235.ec2.internal>
2020-09-17 15:46:31 +0000

    IMPLEMENTING Q AND SARSA   Changes to be committed: 	new file:   .ipynb_checkpoints/SARSA_taxiv2-checkpoint.py 	new file:   SARSA_taxiv2.py

---
+++ .ipynb_checkpoints/SARSA_taxiv2-checkpoint.py
@@ -0,0 +1,116 @@
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+
+def eps_greedy(Q, s, eps=0.1):
+    '''
+    Epsilon Greedy Policy without Exponential Decay.
+    '''
+    if np.random.uniform(0, 1)< eps:
+        return np.random.randint(Q.shape[1])
+    else:
+        return np.argmax(Q[s])
+    
+    
+def run_episodes(env, Q, num_episodes=100, to_print=False):
+    """
+    Executes Episodic Tasks untill termination and returns the total reward at the end. 
+    env: OpenAI env.
+    Q :  Action Value Table 
+    num_episodes: Episodes of Games to play. 
+    to_print: flag for printing stats.
+    """
+    state = env.reset()
+    total_rewards = []
+    
+    for ii in range(num_episodes):
+        done = False
+        curr_reward = 0
+        while not done:
+            s_, r, done, info = env.step(np.argmax(Q[state]))
+            
+            state = s_
+            curr_reward += r
+            if done:
+                state = env.reset()
+                total_rewards.append(curr_reward)
+                
+    if to_print:
+        print(f'Mean Score {np.mean(total_rewards)} of {num_episodes} Episodes')
+        
+    return np.mean(total_rewards)
+
+
+def Q_learning(env, lr=0.01, num_episodes=10000, eps=0.3, gamma=0.95, eps_decay=0.00005):
+    action_range = env.action_space.n
+    observation_range = env.observation_space.n
+    
+    Q = np.zeros((observation_range, action_range))
+    
+    current_reward = []
+    test_rewards = []
+    
+    for ep in range(num_episodes):
+        state = env.reset()
+        done = False
+        total_rewards = 0
+        
+        if eps > 0.01:
+            eps -= eps_decay
+            
+        while not done:
+            action = eps_greedy(Q, state, eps)
+            s_, r, done, info = env.step(action)
+            
+            Q[state][action] += lr * (r + gamma * np.max(Q[s_]) - Q[state][action])
+            state = s_
+            total_rewards += r
+            if done:
+                current_reward.append(total_rewards)
+        
+        if (ep % 300) == 0:
+            test_reward = run_episodes(env, Q, 1000)
+            print("Episode:{:5d}  Epsilon:{:2.4f}  Reward:{:2.4f}".format(ep, eps, test_reward))
+            test_rewards.append(test_reward)
+            
+    return Q
+
+def SARSA(env, lr=0.01, num_episodes=10000, eps=0.3, gamma=0.95, eps_decay=0.00005):
+    action_range = env.action_space.n
+    observation_range = env.observation_space.n
+    Q = np.zeros((observation_range, action_range))
+    games_rewards = []
+    test_rewards = []
+    
+    for ep in range(num_episodes):
+        state = env.reset()
+        done = False
+        total_rewards = 0
+        
+        if eps > 0.01:
+            eps -= eps_decay
+            
+        action = eps_greedy(Q, state, eps)
+        
+        while not done:
+            s_, r, done, info = env.step(action)
+            next_action = eps_greedy(Q, s_, eps)
+            
+            Q[state][action] += lr * (r + gamma * Q[s_][next_action] - Q[state][action])
+            state = s_
+            action = next_action
+            total_rewards += r
+            if done:
+                games_rewards.append(total_rewards)
+        
+        if ep % 300 == 0:
+            test_reward = run_episodes(env, Q, 1000)
+            print("Episode:{:5d}  Eps:{:2.4f}  Rew:{:2.4f}".format(ep, eps, test_reward))
+            test_rewards.append(test_reward)
+            
+    return Q
+if __name__ == '__main__':
+    env = gym.make('Taxi-v3')
+    q_learning = Q_learning(env, lr=0.1, num_episodes=5000, eps=0.4, gamma=.95, eps_decay=.001)
+    q_sarsa = SARSA(env, lr=.1, num_episodes=5000, eps=.4, gamma=.95, eps_decay=.001)
+        
